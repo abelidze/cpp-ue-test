06d98f603e6f1dfd4f0da93b464514b904d6ee14
Abelidze <avataraang399@gmail.com>
2024-10-18 19:24:09 +0200

    Add FitBackgroundToActor function to scale plane according to Text3D

--- Unreal/Source/ErizosTest/ExamActor.cpp
+++ Unreal/Source/ErizosTest/ExamActor.cpp
@@ -8,20 +8,38 @@ AExamActor::AExamActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void AExamActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void AExamActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
+void AExamActor::FitBackgroundToActor(USceneComponent* Background, FCustomMargin Margin)
+{
+	if (!Background)
+	{
+		return;
+	}
+	if (Background->IsCollisionEnabled())
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("[FitBackgroundToActor] Colisions for background should be turned off!"));
+	}
+	FVector BBoxOrigin;
+	FVector BBoxExtent;
+	GetActorBounds(true, BBoxOrigin, BBoxExtent);
+	// CalculateComponentsBoundingBoxInLocalSpace gives us bbox independent of actor's rotation
+	BBoxExtent = CalculateComponentsBoundingBoxInLocalSpace(false).GetExtent();
+	FVector2D&& MarginSize = Margin.GetDesiredSize();
+	BBoxExtent = FVector(BBoxExtent.Z + MarginSize.Y, BBoxExtent.Y + MarginSize.X, BBoxExtent.X) * ScaleFactor;
+	Background->SetRelativeScale3D(BBoxExtent);
+	FVector&& MarginOffset = GetTransform().Inverse().InverseTransformVectorNoScale(FVector(0.f, Margin.Right - Margin.Left, Margin.Top - Margin.Bottom));
+	Background->SetWorldLocation(BBoxOrigin + MarginOffset, false, nullptr, ETeleportType::TeleportPhysics);
+}
--- Unreal/Source/ErizosTest/ExamActor.h
+++ Unreal/Source/ErizosTest/ExamActor.h
@@ -6,12 +6,44 @@
 #include "GameFramework/Actor.h"
 #include "ExamActor.generated.h"
 
-UCLASS()
+USTRUCT(BlueprintType)
+struct FCustomMargin
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Left;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Top;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Right;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Bottom;
+
+public:
+	FCustomMargin()
+		: Left(0.f)
+		, Top(0.f)
+		, Right(0.f)
+		, Bottom(0.f)
+	{ }
+
+public:
+	FVector2D GetDesiredSize() const
+	{
+		return FVector2D(Left + Right, Top + Bottom);
+	}
+};
+
+UCLASS(Blueprintable, BlueprintType)
 class ERIZOSTEST_API AExamActor : public AActor
 {
 	GENERATED_BODY()
 	
-public:	
+public:
 	// Sets default values for this actor's properties
 	AExamActor();
 
@@ -23,4 +55,9 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION(BlueprintCallable)
+	void FitBackgroundToActor(class USceneComponent* Background, FCustomMargin Margin);
+
+private:
+	static constexpr float ScaleFactor = 0.02f;
 };
